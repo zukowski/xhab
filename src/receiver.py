7b869efdeb2876647712318e0664baccd7a95ece
scottmishra <scottmishra@gmail.com>
2013-06-20 12:04:21 -0500

    changed wrist movement and LinWater msg

--- src/receiver.py
+++ src/receiver.py
@@ -19,12 +19,14 @@ wrist = Robotis_Servo( USB1, 5 )
 ee = Robotis_Servo( USB1, 6 )
 
 def callback(data): 
+     #Read in wrist position
+     wrist_angle = wrist.read_angle();
      #Move each of the arm joints to recieved angle
      shoulder1.move_angle( math.radians( float( data.shoulder1_angle ) ) , blocking = False )
-     shoulder2.move_angle( math.radians( float( data.shoulder2_angle ) ) , blocking = True )
+     shoulder2.move_angle( math.radians( float( data.shoulder2_angle ) ) , blocking = False )
      elbow1.move_angle( math.radians( float( data.elbow1_angle ) ) , blocking = True )
      elbow2.move_angle( math.radians(float( data.elbow2_angle ) ) , blocking = True )
-     wrist.move_angle( wrist.read_angle() + math.radians( float( data.wrist_angle ) ) , blocking = True )
+     wrist.move_angle( math.radians( wrist_angle + float( data.wrist_angle ) ) , blocking = True )
  
 def listener():
     rospy.init_node('control_receive_arm_angles', anonymous=True)
